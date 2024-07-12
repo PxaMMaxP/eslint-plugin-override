8de294f52e6246aacbc59169b78f478b725c89cb
Max P <Mail@MPassarello.de>
2024-07-12 16:37:57 +0200

    Update dependencies and remove postinstall script
    - Replaced `@typescript-eslint/experimental-utils` with `@typescript-eslint/utils` to use the current package.
    - Updated version numbers for various dependencies to follow their latest releases and ensure compatibility.
    - Removed the `postinstall.js` script to eliminate the extra npm install step, simplifying the installation process and reducing potential errors.
    - Adjusted package licensing to reflect accurate project details and compliance with published packages.

--- postinstall.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-const path = require('path');
-// Only run the script when executed in the root directory
-if (process.cwd() === path.resolve(__dirname)) {
-    console.log('Running npm install --legacy-peer-deps...');
-    execSync('npm install --legacy-peer-deps', { stdio: 'inherit' });
-} else {
-    console.log('Skipping npm install --legacy-peer-deps...');
-}
